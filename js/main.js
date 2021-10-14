ded76601454bd4a557fcbaf271ddce5185dd8f11
Dragovich <nike_nrj@mail.ru>
2021-10-15 01:38:19 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const backSlide = () => {
+    const elements = document.getElementsByClassName('task4_field')
+    for (let i=0, length = elements.length; i < length; i++) {
+        if (elements[i].classList.contains('active')) {
+            elements[i].classList.remove('active')
+            i--
+            if (i<0) {
+                i = elements.length - 1
+            }
+            elements[i].classList.add('active')
+            return true
+        }
+    }
+}
+
+const nextSlide = () => {
+    const elements = document.getElementsByClassName('task4_field')
+    for (let i=0, length = elements.length; i < length; i++) {
+        if (elements[i].classList.contains('active')) {
+            elements[i].classList.remove('active')
+            i++
+            if (i ===elements.length) {
+                i = 0
+            }
+            elements[i].classList.add('active')
+            return true
+        }
+    }
+}
